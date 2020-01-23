555e22a82d0585e82170a16b63844051ee65b881
Muhammad Abdullah <abdullahnaseer999@gmail.com>
2020-01-23 15:44:11 +0500

    MInor Fix

--- app/Models/Moderator.php
+++ app/Models/Moderator.php
@@ -7,11 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
 
 class Moderator extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
